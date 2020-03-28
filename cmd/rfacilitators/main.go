a4f49deb9fbb50ad43fcf5523834dcce6accc6bd
Marco Paulo Ollivier <marcopollivier@gmail.com>
2020-03-28 02:12:48 -0300

    Add User json parser logic

--- cmd/rfacilitators/main.go
+++ cmd/rfacilitators/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Users struct {
+	Users []User `json:users`
+}
+
+type User struct {
+	Id      int64  `json:"id"`
+	Name    string `json:"name"`
+	Present bool   `json:"present"`
+}
 
 func main() {
-	fmt.Println("Olá, Mundo!")
+
+	parseUsers()
+
+}
+
+func parseUsers() {
+	var jsonFile, err = os.Open("assets/in/users.json")
+	if err != nil {
+		log.Println(err)
+	}
+	defer jsonFile.Close()
+
+	var jsonBytes, _ = ioutil.ReadAll(jsonFile)
+
+	var users Users
+	json.Unmarshal(jsonBytes, &users)
+
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Println("User Id: " + strconv.FormatInt(users.Users[i].Id, 10))
+		fmt.Println("User Name: " + users.Users[i].Name)
+		fmt.Println("User Present: " + users.Users[i].Name)
+	}
 }
