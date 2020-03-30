d57d919958d60dd2d553f14c9f878a6d0325b64a
Marco Paulo Ollivier <marcopollivier@gmail.com>
2020-03-30 20:45:10 -0300

    Read json files

--- cmd/rfacilitators/main.go
+++ cmd/rfacilitators/main.go
@@ -4,42 +4,67 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"strconv"
 )
 
 type Users struct {
-	Users []User `json:users`
+	Users []User `json:"users"`
 }
 
 type User struct {
-	Id      int64  `json:"id"`
+	Id      int    `json:"id"`
 	Name    string `json:"name"`
 	Present bool   `json:"present"`
 }
 
+type Roles struct {
+	Roles []Role `json:"roles"`
+}
+
+type Role struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
 
-	parseUsers()
+	fmt.Println("")
+
+	var users = returnAllUsers()
+	for i := 0; i < len(users.Users); i++ {
+		fmt.Print("User Id: " + strconv.Itoa(users.Users[i].Id) + " | ")
+		fmt.Print("User Name: " + users.Users[i].Name + " | ")
+		fmt.Println("User Present: " + strconv.FormatBool(users.Users[i].Present))
+	}
+
+	fmt.Println("")
+
+	var roles = returnAllRoles()
+	for i := 0; i < len(roles.Roles); i++ {
+		fmt.Print("User Id: " + strconv.Itoa(roles.Roles[i].Id) + " | ")
+		fmt.Println("User Name: " + roles.Roles[i].Name)
+	}
 
 }
 
-func parseUsers() {
-	var jsonFile, err = os.Open("assets/in/users.json")
-	if err != nil {
-		log.Println(err)
-	}
+func returnAllUsers() Users {
+	var users Users
+	unmarshalFromFile("users.json", &users)
+	return users
+}
+
+func returnAllRoles() Roles {
+	var roles Roles
+	unmarshalFromFile("roles.json", &roles)
+	return roles
+}
+
+func unmarshalFromFile(assetFilename string, dataStructure interface{}) error {
+	var jsonFile, _ = os.Open("assets/in/" + assetFilename)
 	defer jsonFile.Close()
 
 	var jsonBytes, _ = ioutil.ReadAll(jsonFile)
 
-	var users Users
-	json.Unmarshal(jsonBytes, &users)
-
-	for i := 0; i < len(users.Users); i++ {
-		fmt.Println("User Id: " + strconv.FormatInt(users.Users[i].Id, 10))
-		fmt.Println("User Name: " + users.Users[i].Name)
-		fmt.Println("User Present: " + users.Users[i].Name)
-	}
+	return json.Unmarshal(jsonBytes, dataStructure)
 }
